004bd0a5efd778a2a9ab9e61b28ae3a0abd634b2
akk0ga <ttn35000@gmail.com>
2021-05-24 10:47:20 +0200

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Csv-To-DB",
+    name="csvtodb",
     version="0.0.1",
     author="akk0ga",
     author_email="ttn.glock@orange.fr",
