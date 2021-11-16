8789728812ad4c63d450ef6c9abe42a85f997d89
akk0ga <45255672+akk0ga@users.noreply.github.com>
2021-11-16 16:43:25 +0100

    add test for column to check type returned by __get_type()

--- unitary_test/mysql/MysqlColumnTest.py
+++ unitary_test/mysql/MysqlColumnTest.py
@@ -4,6 +4,11 @@ from csvtodb.mysql.MysqlColumn import MysqlColumn
 
 class MysqlColumnTest(unittest.TestCase):
 
+    """
+    ================================================
+    test method to check type
+    ================================================
+    """
     def test_is_int(self):
         column = MysqlColumn(name='', value=['15', '16', '17', '18'])
         self.assertEqual('int', column.type, 'test passed')  # add assertion here
@@ -31,6 +36,35 @@ class MysqlColumnTest(unittest.TestCase):
     def test_has_null(self):
         column = MysqlColumn(name='', value=['45', '', '50', '45y'])
         self.assertTrue(column.has_null())
+
+    """
+    ================================================
+    test type set for column
+    ================================================
+    """
+    def test_type_is_float(self):
+        column = MysqlColumn(name='', value=['45.25', '', '50,52', '54'])
+        self.assertEqual('float', column.type)
+
+    def test_type_is_enum(self):
+        column = MysqlColumn(name='enum_name', value=[])
+        self.assertEqual('enum', column.type)
+
+    def test_type_is_int(self):
+        column = MysqlColumn(name='', value=['45', '54', '58'])
+        self.assertEqual('int', column.type)
+
+    def test_type_is_str(self):
+        column = MysqlColumn(name='', value=['stre', '45', ''])
+        self.assertEqual('str', column.type)
+
+    def test_type_is_pk(self):
+        column = MysqlColumn(name='pk_name', value=[])
+        self.assertEqual('pk', column.type)
+
+    def test_type_is_fk(self):
+        column = MysqlColumn(name='fk_name', value=[])
+        self.assertEqual('fk', column.type)
 
 
 if __name__ == '__main__':
